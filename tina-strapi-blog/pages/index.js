fd631e881755f6b63c799d92105c52870d390ccd
Bryan Hidalgo <bryan.hidalgo@autodesk.com>
2020-12-08 12:34:58 -0500

    test

--- tina-strapi-blog/pages/index.js
+++ tina-strapi-blog/pages/index.js
@@ -6,13 +6,13 @@ import Layout from "../components/layout";
 
 import Head from "next/head";
 import { CMS_NAME } from "../lib/constants";
-import { fetchGraphql } from "react-tinacms-strapi";
+// import { fetchGraphql } from "react-tinacms-strapi";
 
 //...
 
 export default function Index({ allPosts }) {
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
+  // const heroPost = allPosts[0];
+  // const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout>
@@ -21,7 +21,7 @@ export default function Index({ allPosts }) {
         </Head>
         <Container>
           <Intro />
-          {heroPost && (
+          {/* {heroPost && (
             <HeroPost
               title={heroPost.title}
               coverImage={process.env.STRAPI_URL + heroPost.coverImage.url}
@@ -31,44 +31,44 @@ export default function Index({ allPosts }) {
               excerpt={heroPost.excerpt}
             />
           )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />} */}
         </Container>
       </Layout>
     </>
   );
 }
 
-export async function getStaticProps({ params, preview, previewData }) {
-  const postResults = await fetchGraphql(
-    process.env.STRAPI_URL,
-    `
-    query{
-      blogPosts {
-        title
-        date
-        slug
-        author {
-          name
-          picture { 
-            url
-          }
-        }
-        excerpt
-        coverImage {
-          url
-        }
-      }
-    }
-  `
-  );
+// export async function getStaticProps({ params, preview, previewData }) {
+//   const postResults = await fetchGraphql(
+//     process.env.STRAPI_URL,
+//     `
+//     query{
+//       blogPosts {
+//         title
+//         date
+//         slug
+//         author {
+//           name
+//           picture {
+//             url
+//           }
+//         }
+//         excerpt
+//         coverImage {
+//           url
+//         }
+//       }
+//     }
+//   `
+//   );
 
-  if (preview) {
-    return {
-      props: { allPosts: postResults.data.blogPosts, preview, ...previewData },
-    };
-  }
+//   if (preview) {
+//     return {
+//       props: { allPosts: postResults.data.blogPosts, preview, ...previewData },
+//     };
+//   }
 
-  return {
-    props: { allPosts: postResults.data.blogPosts, preview: false },
-  };
-}
+//   return {
+//     props: { allPosts: postResults.data.blogPosts, preview: false },
+//   };
+// }
