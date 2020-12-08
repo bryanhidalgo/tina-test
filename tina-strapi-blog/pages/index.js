f49f041fa73bf7967db4686dda3b2bb77e811d2e
Bryan Hidalgo <bryan.hidalgo@autodesk.com>
2020-12-08 11:13:21 -0500

    index revert

--- tina-strapi-blog/pages/index.js
+++ tina-strapi-blog/pages/index.js
@@ -10,16 +10,65 @@ import { fetchGraphql } from "react-tinacms-strapi";
 
 //...
 
-export default function Index() {
-  // const heroPost = allPosts[0];
-  // const morePosts = allPosts.slice(1);
+export default function Index({ allPosts }) {
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with </title>
+          <title>Next.js Blog Example with {CMS_NAME}</title>
         </Head>
+        <Container>
+          <Intro />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={process.env.STRAPI_URL + heroPost.coverImage.url}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
       </Layout>
     </>
   );
+}
+
+export async function getStaticProps({ params, preview, previewData }) {
+  const postResults = await fetchGraphql(
+    process.env.STRAPI_URL,
+    `
+    query{
+      blogPosts {
+        title
+        date
+        slug
+        author {
+          name
+          picture { 
+            url
+          }
+        }
+        excerpt
+        coverImage {
+          url
+        }
+      }
+    }
+  `
+  );
+
+  if (preview) {
+    return {
+      props: { allPosts: postResults.data.blogPosts, preview, ...previewData },
+    };
+  }
+
+  return {
+    props: { allPosts: postResults.data.blogPosts, preview: false },
+  };
 }
