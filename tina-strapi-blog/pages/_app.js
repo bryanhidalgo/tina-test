97ea0a079eca9bea845f4c425326140b5e834d9c
Bryan Hidalgo <bryan.hidalgo@autodesk.com>
2020-12-08 13:28:10 -0500

    test

--- tina-strapi-blog/pages/_app.js
+++ tina-strapi-blog/pages/_app.js
@@ -9,40 +9,40 @@ import { TinaCMS, TinaProvider } from "tinacms";
 import { useCMS } from "@tinacms/react-core";
 import { useMemo } from "react";
 
-export const EditButton = () => {
-  const cms = useCMS();
-  return (
-    <button onClick={() => (cms.enabled ? cms.disable() : cms.enable())}>
-      {cms.enabled ? `Stop Editing ` : `Edit this Site `}
-    </button>
-  );
-};
+// export const EditButton = () => {
+//   const cms = useCMS();
+//   return (
+//     <button onClick={() => (cms.enabled ? cms.disable() : cms.enable())}>
+//       {cms.enabled ? `Stop Editing ` : `Edit this Site `}
+//     </button>
+//   );
+// };
 
-const enterEditMode = () => {
-  return fetch(`/api/preview`).then(() => {
-    window.location.href = window.location.pathname;
-  });
-};
+// const enterEditMode = () => {
+//   return fetch(`/api/preview`).then(() => {
+//     window.location.href = window.location.pathname;
+//   });
+// };
 
-const exitEditMode = () => {
-  return fetch(`/api/reset-preview`).then(() => {
-    window.location.reload();
-  });
-};
+// const exitEditMode = () => {
+//   return fetch(`/api/reset-preview`).then(() => {
+//     window.location.reload();
+//   });
+// };
 
 export default function MyApp({ Component, pageProps }) {
-  const cms = useMemo(
-    () =>
-      new TinaCMS({
-        toolbar: pageProps.preview,
-        enabled: pageProps.preview,
-        apis: {
-          strapi: new StrapiClient(process.env.STRAPI_URL),
-        },
-        media: new StrapiMediaStore(process.env.STRAPI_URL),
-      }),
-    []
-  );
+  // const cms = useMemo(
+  //   () =>
+  //     new TinaCMS({
+  //       toolbar: pageProps.preview,
+  //       enabled: pageProps.preview,
+  //       apis: {
+  //         strapi: new StrapiClient(process.env.STRAPI_URL),
+  //       },
+  //       media: new StrapiMediaStore(process.env.STRAPI_URL),
+  //     }),
+  //   []
+  // );
   console.log(process.env.STRAPI_URL);
   return (
     <>
