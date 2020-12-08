0bc27bb66d1bbb2fcd08f4ece9d76d86ebf32239
Bryan Hidalgo <bryan.hidalgo@autodesk.com>
2020-12-08 13:04:57 -0500

    revrt app

--- tina-strapi-blog/pages/_app.js
+++ tina-strapi-blog/pages/_app.js
@@ -5,8 +5,52 @@ import {
   StrapiProvider,
   StrapiClient,
 } from "react-tinacms-strapi";
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import { TinaCMS, TinaProvider } from "tinacms";
+import { useCMS } from "@tinacms/react-core";
+import { useMemo } from "react";
 
-export default MyApp;
+// ...
+
+export const EditButton = () => {
+  const cms = useCMS();
+  return (
+    <button onClick={() => (cms.enabled ? cms.disable() : cms.enable())}>
+      {cms.enabled ? `Stop Editing ` : `Edit this Site `}
+    </button>
+  );
+};
+
+const enterEditMode = () => {
+  return fetch(`/api/preview`).then(() => {
+    window.location.href = window.location.pathname;
+  });
+};
+
+const exitEditMode = () => {
+  return fetch(`/api/reset-preview`).then(() => {
+    window.location.reload();
+  });
+};
+
+export default function MyApp({ Component, pageProps }) {
+  const cms = useMemo(
+    () =>
+      new TinaCMS({
+        toolbar: pageProps.preview,
+        enabled: pageProps.preview,
+        apis: {
+          strapi: new StrapiClient(process.env.STRAPI_URL),
+        },
+        media: new StrapiMediaStore(process.env.STRAPI_URL),
+      }),
+    []
+  );
+  return (
+    <TinaProvider cms={cms}>
+      <StrapiProvider onLogin={enterEditMode} onLogout={exitEditMode}>
+        <EditButton />
+        <Component {...pageProps} />
+      </StrapiProvider>
+    </TinaProvider>
+  );
+}
