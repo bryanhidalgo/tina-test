956fa5fd44bf3099632d544d907fdc80f8daa7f6
Bryan Hidalgo <bryan.hidalgo@autodesk.com>
2020-12-08 11:08:26 -0500

    test fetch

--- tina-strapi-blog/pages/index.js
+++ tina-strapi-blog/pages/index.js
@@ -19,56 +19,7 @@ export default function Index({ allPosts }) {
         <Head>
           <title>Next.js Blog Example with {CMS_NAME}</title>
         </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={process.env.STRAPI_URL + heroPost.coverImage.url}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
       </Layout>
     </>
   );
-}
-
-export async function getStaticProps({ params, preview, previewData }) {
-  const postResults = await fetchGraphql(
-    process.env.STRAPI_URL,
-    `
-    query{
-      blogPosts {
-        title
-        date
-        slug
-        author {
-          name
-          picture { 
-            url
-          }
-        }
-        excerpt
-        coverImage {
-          url
-        }
-      }
-    }
-  `
-  );
-
-  if (preview) {
-    return {
-      props: { allPosts: postResults.data.blogPosts, preview, ...previewData },
-    };
-  }
-
-  return {
-    props: { allPosts: postResults.data.blogPosts, preview: false },
-  };
 }
